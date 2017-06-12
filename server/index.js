41f430e18789891b2d049704e9437a67136b8452
Phillip_Chan <phillip.chan@liferay.COM>
2017-06-12 14:59:19 -0700

    SSL test

--- server/index.js
+++ server/index.js
@@ -28,10 +28,10 @@ app.use('/web-hooks/', webhookRoutes);
 //         .sendFile(process.cwd() + '/client/index.html');
 // });
 
-var filename = '/.well-known/acme-challenge/jEbdsh56SXs5jNYZA6M7GcBPrITcKdOlIT7o01scDdE';
+var filename = '/.well-known/acme-challenge/IacNUMZjwHaglJc8E5SZt1Jv8mJbInNMH6--v9r5hus';
 
 app.all(filename, function(req, res) {
-	fs.readFile('.well-known/acme-challenge/jEbdsh56SXs5jNYZA6M7GcBPrITcKdOlIT7o01scDdE', "binary", function(err, file) {
+	fs.readFile('.well-known/acme-challenge/IacNUMZjwHaglJc8E5SZt1Jv8mJbInNMH6--v9r5hus', "binary", function(err, file) {
       if(err) {
         res.writeHead(500, {"Content-Type": "text/plain"});
         res.write(err + "\n");
