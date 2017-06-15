2fdb12ba315519c2b3fb43b0b5f50e266505ef94
Phillip_Chan <phillip.chan@liferay.COM>
2017-06-15 13:41:16 -0700

    Testing throttle API attempts

--- server/web-hooks/web-hooks.routes.js
+++ server/web-hooks/web-hooks.routes.js
@@ -4,24 +4,34 @@ var regionConverter = require('../regionConverter/regionConverter');
 
 router.post('/', function(req, res, callback) {
 	var payload = req.body;
+	var numOfPayloads = payload.length;
+	var occurences = 0;
 
-	console.log(payload);
-
+	// throttle number of api calls with custom loop
 	if (payload) {
-		for (let p = 0; p < payload.length; p++) {
-			let event = payload[p].subscriptionType;
+		var interval = setInterval(function() {
+			if (occurences < numOfPayloads) {
+				console.log(payload[occurences]);
 
-			if (event === 'contact.creation') {
-				console.log('New Contact Created');
-				regionConverter.addRegionToContact(payload[p].objectId);
-			}
+				let event = payload[occurences].subscriptionType;
 
-			// if a contact changes a specific property
-			else if (event === 'contact.propertyChange') {
-				console.log('Contact Property Change');
-				regionConverter.addRegionToContact(payload[p].objectId);
+				if (event === 'contact.creation') {
+					console.log('New Contact Created');
+					regionConverter.addRegionToContact(payload[p].objectId);
+				}
+
+				// if a contact changes a specific property
+				else if (event === 'contact.propertyChange') {
+					console.log('Contact Property Change');
+					regionConverter.addRegionToContact(payload[p].objectId);
+				}
+
+				occurences++;
 			}
-		}
+			else {
+				clearInterval(interval);
+			}
+		}, 1000);
 	}
 });
 
