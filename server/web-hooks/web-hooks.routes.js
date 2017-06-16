1dd4de0352b338c5385d8141d6fa126c5a8f8dc4
Phillip_Chan <phillip.chan@liferay.COM>
2017-06-16 12:10:50 -0700

    Allow more payload types in webhook

--- server/web-hooks/web-hooks.routes.js
+++ server/web-hooks/web-hooks.routes.js
@@ -7,8 +7,8 @@ router.post('/', function(req, res, callback) {
 	var numOfPayloads = payload.length;
 	var occurences = 0;
 
-	if (payload) {
-
+	// if payload is JSON object
+	if (Array.isArray(payload)) {
 		// throttle number of api calls with custom loop
 		var interval = setInterval(function() {
 			if (occurences < numOfPayloads) {
@@ -45,6 +45,14 @@ router.post('/', function(req, res, callback) {
 				clearInterval(interval);
 			}
 		}, 3000);
+	}
+
+	// if payload is single object
+	else if (typeof payload === 'object') {
+		regionConverter.addRegionToContact(payload.vid);
+	}
+	else {
+		console.log('Not a proper payload datatype');
 	}
 });
 
