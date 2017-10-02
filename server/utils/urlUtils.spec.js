7e3470eec3ed152a07244b87bdfa84feb093c469
Phillip_Chan <phillip.chan@liferay.COM>
2017-10-02 10:39:46 -0700

    Using a scheduler instsead of setInterval

---
+++ server/utils/urlUtils.spec.js
@@ -0,0 +1,11 @@
+var urlUtils = require('./urlUtils');
+var should = require('chai').should();
+
+describe('serializeQueryParameters', function() {
+	it('should make a query parameter list', function() {
+		var expected;
+		var actual;
+
+		actual.should.equal(expected);
+	});
+});
