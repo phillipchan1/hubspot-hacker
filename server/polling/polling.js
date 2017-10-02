25000adbad632a5a794486c08e4fd9e131876ed0
Phillip_Chan <phillip.chan@liferay.COM>
2017-10-02 10:18:23 -0700

    Using a scheduler instsead of setInterval

--- server/polling/polling.js
+++ server/polling/polling.js
@@ -1,9 +1,18 @@
 let config = require('../../config/config');
 let regionConverterPollImpl = require('./regionConverterPollImpl');
+var schedule = require('node-schedule');
+
+var regionConverterPoll = new schedule.RecurrenceRule();
+regionConverterPoll.dayOfWeek = [0, 6];
+regionConverterPoll.hour = 6;
+regionConverterPoll.minute = 0;
 
 regionConverterPollImpl();
+var j = schedule.scheduleJob(regionConverterPoll, function(){
+  regionConverterPollImpl();
+});
 
 // run a poll to clean up contacts without a region
-var regionConverterPoll = setInterval(function() {
-	regionConverterPollImpl();
-}, config.polling_frequency);
+// var regionConverterPoll = setInterval(function() {
+// 	regionConverterPollImpl();
+// }, config.polling_frequency);
