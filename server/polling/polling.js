b4f67bdd8f12a85e13360c55c29eaacd03bb5643
Phillip_Chan <phillip.chan@liferay.COM>
2017-10-05 10:57:37 -0700

    Refresh Tokens

--- server/polling/polling.js
+++ server/polling/polling.js
@@ -7,13 +7,8 @@ regionConverterPoll.dayOfWeek = [new schedule.Range(0, 6)];
 regionConverterPoll.hour = 6;
 regionConverterPoll.minute = 0;
 
-regionConverterPollImpl();
+// regionConverterPollImpl();
 var j = schedule.scheduleJob(regionConverterPoll, function(){
 	console.log(`Starting regionConverterPoll CRON Job on ${new Date().toJSON().slice(0,10).replace(/-/g,'/')} at ${new Date().toLocaleTimeString()}`);
 	regionConverterPollImpl();
 });
-
-// run a poll to clean up contacts without a region
-// var regionConverterPoll = setInterval(function() {
-// 	regionConverterPollImpl();
-// }, config.polling_frequency);
